542875d569c27332f284bdf3e3ed9fc3ed1447bf
akshay23sep <akshayanand153@gmail.com>
2021-06-10 10:48:42 -0400

    remove all

--- intern-prompt/runall.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cp -r 0.org 0
-blockMesh > log.blockMesh
-surfaceFeatureExtract > log.surfaceFeatureExtract
-snappyHexMesh -overwrite > log.snappyHexMesh
-simpleFoam > log.simpleFoam
-
